db86e7256834e6b380b91083d61850b713fcb350
Vaishnav Arora <vaishgabha@gmail.com>
2023-08-22 23:14:28 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "spaceship",
+    image: "QmYfrwwDAkpBjwrEF9Kst9vQ3mhs3BXMeV7aUtmh11R88G",
+  },
+  {
+    name: "1",
+    description: "spaceshuttel",
+    image: "QmTU4UckkvVzcE6GrYj7Pg1NA52etnMPGd2ihpEdMzjKdS",
+  }, //change
+  {
+    name: "2",
+    description: "lambo",
+    image: "QmdsCiYw7ekMmTbLYskfNekPsPpdWLX4z8Sjmr9itt4t4N",
+  },
+  {
+    name: "3",
+    description: "chess board",
+    image: "QmbP1tcnrKsAMCAotjT4yy4Ns6S6ZhNq3AggWcDzZHiaRY",
+  },
+  {
+    name: "4",
+    description: "truck.",
+    image: "QmQ3yALVecYKSMfuPBRaowR7HdLmZWp9fQGfKFP9GyAYC4",
+  },
+];
+
+module.exports = nfts;
